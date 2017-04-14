78f0393b3b2fc1c94a051b05368b520d3381cab5
Paul Davis <paul@linuxaudiosystems.com>
2017-04-14 17:32:16 +0100

    correctly create Tracks, not Busses

--- libs/ardour/session_state.cc
+++ libs/ardour/session_state.cc
@@ -1722,10 +1722,10 @@ Session::XMLRouteFactory (const XMLNode& node, int version)
 		return ret;
 	}
 
-	XMLNode* pl_child = find_named_node (node, X_("audio-playlist"));
+	XMLProperty const * pl_prop = node.property (X_("audio-playlist"));
 
-	if (!pl_child) {
-		pl_child = find_named_node (node, X_("midi-playlist"));
+	if (!pl_prop) {
+		pl_prop = node.property (X_("midi-playlist"));
 	}
 
 	DataType type = DataType::AUDIO;
@@ -1737,13 +1737,17 @@ Session::XMLRouteFactory (const XMLNode& node, int version)
 
 	assert (type != DataType::NIL);
 
-	if (pl_child) {
+	if (pl_prop) {
+
+		/* has at least 1 playlist, therefore a track ... */
 
 		boost::shared_ptr<Track> track;
 
                 if (type == DataType::AUDIO) {
+	                cerr << "Creating a new audio track\n";
                         track.reset (new AudioTrack (*this, X_("toBeResetFroXML")));
                 } else {
+	                cerr << "Creating a new MIDI track\n";
                         track.reset (new MidiTrack (*this, X_("toBeResetFroXML")));
                 }
 
@@ -1760,6 +1764,7 @@ Session::XMLRouteFactory (const XMLNode& node, int version)
 
 	} else {
 		PresentationInfo::Flag flags = PresentationInfo::get_flags (node);
+		cerr << "Creating a new bus\n";
 		boost::shared_ptr<Route> r (new Route (*this, X_("toBeResetFroXML"), flags));
 
                 if (r->init () == 0 && r->set_state (node, version) == 0) {
